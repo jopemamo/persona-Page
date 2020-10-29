2d7c283880a9e1f7ccb3bf5472c160d4152d12e6
Jorge Moreno <jpmmpj@gmail.com>
2020-10-29 12:45:58 +0100

    router added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
+    <BrowserRouter>
+    <Switch>
+      <Route path='/' render={() => (
+        <p>Press Enter to Start</p>
+      )}>
+      </Route>
+    </Switch>
+    </BrowserRouter>
     </>
   )
 }
