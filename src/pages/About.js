a4ae634c5b38c27b1a4f807982803befd0f7ebcf
Jorge Moreno <jpmmpj@gmail.com>
2020-11-02 13:24:10 +0100

    redirect to projects from about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {  Link, Redirect } from 'react-router-dom';
+
+import KeyHandler from '../components/KeyHandler'
 
 const About = () => {
-  return(
-    <p>This is about</p>
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleKeyPressed = () => {
+    setSubmitted(true)
+  }
+  if (submitted === true) {
+    return (
+      <Redirect to="/projects" />
+    )
+  } else {
+  return (
+    <>
+      <p>This is about</p>
+      <Link className="enter" to='/projects' style={{ textDecoration: 'none' }}>
+        <p >Press Enter to Start</p>
+        <KeyHandler handleKeyPressed={handleKeyPressed} />
+      </Link>
+    </>
   )
+  }
 }
 
 export default About;
