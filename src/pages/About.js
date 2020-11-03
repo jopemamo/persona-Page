fcd3a71150b01dbe3b56a47774ad086edb76d2c9
Jorge Moreno <jpmmpj@gmail.com>
2020-11-03 17:18:18 +0100

    typewritting for html

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
 import KeyHandler from '../components/KeyHandler'
@@ -9,8 +9,23 @@ const About = () => {
   const handleKeyPressed = () => {
     setSubmitted(true)
   }
+  useEffect(()=> {
+    typeWriter();
+  },[])
+
+  let i = 0;
+  const txt = 'After walking several paths and living different experiences, our Hero was ready for the definitive adventure.';
+  const speed = 100;
+
+  const typeWriter = () => {
+    if (i < txt.length) {
+      document.getElementById("text").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
   return (
-    <>
+    <main>
       {
         submitted ?
           <Redirect to="/projects" />
@@ -18,12 +33,13 @@ const About = () => {
           <>
             <h1>Jorge's Adventure</h1>
             <img src="https://res.cloudinary.com/diggrhtle/image/upload/v1604332718/persona%20page/Jorge_Moreno_2_z9iur1.jpg"></img>
-            <Link  to='/projects' style={{ textDecoration: 'none' }}>
+            <p id="text"></p>
+            <Link to='/projects' style={{ textDecoration: 'none' }}>
               <p className="options" >Press Enter to Continue</p>
               <KeyHandler handleKeyPressed={handleKeyPressed} />
             </Link>
           </>}
-    </>
+    </ main>
   )
 }
 
