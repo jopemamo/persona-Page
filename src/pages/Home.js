25ef548fd4767d0fdab2b6c828391bf9013eaa14
Jorge Moreno <jpmmpj@gmail.com>
2020-11-05 15:14:04 +0100

    toggling sound

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,14 @@ import Projects from './Projects';
 import KeyHandler from '../components/KeyHandler';
 
 const Home = () => {
-  const [submitted, setSubmitted] = useState(false)
+  const [submitted, setSubmitted] = useState(false);
+  const [sound, setSound] = useState(true);
 
   const handleKeyPressed = () => {
     setSubmitted(true)
+  }
+  const handleClick = () => {
+    setSound(!sound);
   }
   return (
     <>
@@ -27,12 +31,19 @@ const Home = () => {
               <Redirect to="/about" />
               :
               <>
-                <h1>Jorge's Adventure</h1>
-                <iframe title="homeAudio" src="https://res.cloudinary.com/diggrhtle/video/upload/v1604576120/persona%20page/title_ig0xau.mp3" type="audio/mp3" allow="autoplay" className="audioFrame"></iframe>
-                <audio autoPlay>
-                  <source src="https://res.cloudinary.com/diggrhtle/video/upload/v1604576120/persona%20page/title_ig0xau.mp3" type="audio/mpeg" />
+                {sound ?
+                  <>
+                    <img onClick={handleClick} className="sound" src="https://res.cloudinary.com/diggrhtle/image/upload/v1604580007/persona%20page/volume_1_u4ri1u.png" alt="soundOn"></img>
+                    <iframe title="homeAudio" src="https://res.cloudinary.com/diggrhtle/video/upload/v1604576120/persona%20page/title_ig0xau.mp3" type="audio/mp3" allow="autoplay" className="audioFrame"></iframe>
+                    <audio autoPlay>
+                      <source src="https://res.cloudinary.com/diggrhtle/video/upload/v1604576120/persona%20page/title_ig0xau.mp3" type="audio/mpeg" />
                     Your browser does not support the audio tag.
                 </audio>
+                  </>
+                  :
+                  <img onClick={handleClick} className="sound" src="https://res.cloudinary.com/diggrhtle/image/upload/v1604584806/persona%20page/mute-button_vtvguw.png" alt="soundOff"></img>
+                }
+                <h1>Jorge's Adventure</h1>
                 <Link className="enter" to='/about' style={{ textDecoration: 'none' }}>
                   <p className="options">Press Enter to Start</p>
                   <KeyHandler handleKeyPressed={handleKeyPressed} />
