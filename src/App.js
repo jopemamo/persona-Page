2e25a0e6d57bcc756390524deaf7d5bba365ae07
Jorge Moreno <jpmmpj@gmail.com>
2020-11-10 13:37:06 +0100

    structure changed to make the Routers in App insteado of Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import Home from './pages/Home'
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 import './styles/App.css';
 
 const App = () => {
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const [sound, setSound] = useState(false);
+
+  const handleKeyPressed = () => {
+    setSubmitted(true)
+  }
+
+  const handleSound = () => {
+    setSound(!sound);
+  }
+
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/about' render={() =>
+            <About handleSound={handleSound} sound={sound} />
+          }>
+          </Route>
+          <Route path='/projects' render={() =>
+            <Projects handleSound={handleSound} sound={sound} />
+          }>
+          </Route>
+          <Route path='/' render={() =>
+            <Home submitted={submitted} handleKeyPressed={handleKeyPressed} handleSound={handleSound} sound={sound} />
+          }
+          >
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,30 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
-import About from './About';
-import Projects from './Projects';
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import KeyHandler from '../components/KeyHandler';
 import Sound from '../components/Sound';
 import audio from '../audios/title.mp3'
 import '../styles/Home.css';
 
-const Home = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const [sound, setSound] = useState(false);
+const Home = ({ submitted, sound, handleSound, handleKeyPressed }) => {
 
   const audioLink = "https://res.cloudinary.com/diggrhtle/video/upload/v1604576120/persona%20page/title_ig0xau.mp3";
 
-  const handleKeyPressed = () => {
-    setSubmitted(true)
-  }
-  const handleSound = () => {
-    setSound(!sound);
-  }
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/about' render={() =>
-            <About handleSound={handleSound} sound={sound} />
-          }>
-          </Route>
-          <Route path='/projects' render={() =>
-            <Projects handleSound={handleSound} sound={sound} />
-          }>
-          </Route>
-          <Route path='/' render={() =>
-            submitted ?
-              <Redirect to="/about" />
-              :
-              <>
-                <Sound sound={sound} audio={audio} audioLink={audioLink} handleSound={handleSound} />
-                  <main className="home__container">
-                    <h1 id="homeTitle">Jorge's Adventure</h1>
-                    {<Link className="enter" to='/about' style={{ textDecoration: 'none' }}>
-                      <p className="options">Press Enter to Start</p>
-                      <KeyHandler handleKeyPressed={handleKeyPressed} />
-                    </Link>}
-                  </ main>
-              </>
-          }
-          >
-          </Route>
-        </Switch>
-      </BrowserRouter>
-
+      {submitted ?
+        <Redirect to="/about" />
+        :
+        <>
+          <Sound sound={sound} audio={audio} audioLink={audioLink} handleSound={handleSound} />
+          <main className="home__container">
+            <h1 id="homeTitle">Jorge's Adventure</h1>
+            {<Link className="enter" to='/about' style={{ textDecoration: 'none' }}>
+              <p className="options">Press Enter to Start</p>
+              <KeyHandler handleKeyPressed={handleKeyPressed} />
+            </Link>}
+          </ main>
+        </>
+      }
     </>
   )
 }
