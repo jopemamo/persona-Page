4d97050d681a9ee877d77f480f2d4d3e349799fa
Jorge Moreno <jpmmpj@gmail.com>
2020-11-14 18:04:26 +0100

    make modals responsive too

--- src/components/TimelineSmall.js
+++ src/components/TimelineSmall.js
@@ -2,23 +2,16 @@ import React from 'react'
 import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from '@material-ui/lab';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '../components/Modal';
-import LoanCalculator from './LoanCalculator';
-import WMW from './WMW';
-import BidraSammen from './BidraSammen';
-import JSToDo from '../components/JSToDo';
-import ReactToDo from '../components/ReactToDo';
 import cv from '../files/CV_Jorge_Moreno.pdf';
 
 const useStyles = makeStyles((theme) => ({
-
   paper: {
     padding: '26px 16px',
     minWidth: "400px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    textAlign:"center"
+    textAlign: "center"
   },
 }));
 
@@ -38,41 +31,41 @@ const TimelineSmall = () => {
       description: "A Loan Calculator done with React.",
       gitHub: "https://github.com/jopemoma/loan-calculator",
       modal: true,
-      component: LoanCalculator,
+      link: "https://agitated-shaw-abd9ca.netlify.app/",
     },
     {
       name: "BidraSammen",
       description: "Our Graduation Project at Salt. An App done with React Native.",
       gitHub: "https://github.com/jopemoma/Graduation-Project",
       modal: true,
-      component: BidraSammen,
+      link: "https://res.cloudinary.com/diggrhtle/video/upload/v1604416370/persona%20page/Cloud0x9_Product_Demo_rjclga.mp4",
     },
     {
       name: "Which Movie is Worse?",
       description: "A game where you need to guess which movie has the worse rating! Done with React",
       gitHub: "https://github.com/jopemoma/Which-movie-is-worse-Game",
       modal: true,
-      component: WMW,
+      link: "https://which-movie-is-worse.herokuapp.com/",
     },
     {
       name: "React ToDo App",
       description: "A ToDo App done with React",
       gitHub: "https://github.com/jopemoma/React-ToDo-app",
       modal: true,
-      component: ReactToDo,
+      link: "https://ecstatic-shirley-082a22.netlify.app",
     },
     {
       name: "JavaScript ToDo App",
       description: "A ToDo App done with Vanilla JavaScript",
       gitHub: "https://github.com/jopemoma/Vanilla-JavaScript-ToDo-App",
       modal: true,
-      component: JSToDo,
+      link: "https://determined-varahamihira-fd0862.netlify.app/",
     },
   ]
 
   return (
     <div className="timeline" >
-      <Timeline align="left" style={{maxWidth: "90%"}}>
+      <Timeline align="left" style={{ maxWidth: "90%" }}>
         {projects.map((project, index) => (
           <TimelineItem key={index}>
             <TimelineOppositeContent>
@@ -88,7 +81,7 @@ const TimelineSmall = () => {
                 <p className="project">{project.description}</p>
                 <p className="project">You can check the repository on GitHub here: <a href={project.gitHub} className="nes-btn is-small is-primary" target="_blank" rel="noreferrer" >{project.name} </a> </p>
                 {project.modal &&
-                  <Modal project={project.component} />
+                  <a href={project.link} className="nes-btn is-success" target="_blank" rel="noreferrer" >Click here to see the Demo! </a>
                 }
               </Paper>
             </TimelineContent>
@@ -96,7 +89,7 @@ const TimelineSmall = () => {
         ))}
         <TimelineItem id="contact">
           <TimelineOppositeContent>
-          <p className="smallStage" >STAGE 0</p>
+            <p className="smallStage" >STAGE 0</p>
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineDot />
@@ -107,19 +100,19 @@ const TimelineSmall = () => {
               <p className="project__title">Contact Information </p>
               <img className="theHero" src="https://res.cloudinary.com/diggrhtle/image/upload/v1604332718/persona%20page/Jorge_Moreno_2_z9iur1.jpg" alt="The Hero" />
               <section className="section__contact" >
-              <a href="https://github.com/jopemoma" target="_blank" rel="noreferrer" >
-                <i className="nes-icon github is-large"></i>
-              </a>
-              <a href="mailto:jorge.moreno@appliedtechnology.se" target="_blank" rel="noreferrer" >
-                <i className="nes-icon gmail is-large"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/jorge-moreno-mateo/" target="_blank" rel="noreferrer" >
-                <i className="nes-icon linkedin is-large"></i>
-              </a>
-              <a className="anchor__CV" href={cv} target="_blank" rel="noreferrer" >
-{/*               <!-- Copyright Nintendo --> */}
-                <img className="nes-avatar link" src="https://res.cloudinary.com/diggrhtle/image/upload/v1605175715/persona%20page/link_cv6kti.gif"  alt="CV" />
-              </a>
+                <a href="https://github.com/jopemoma" target="_blank" rel="noreferrer" >
+                  <i className="nes-icon github is-large"></i>
+                </a>
+                <a href="mailto:jorge.moreno@appliedtechnology.se" target="_blank" rel="noreferrer" >
+                  <i className="nes-icon gmail is-large"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/jorge-moreno-mateo/" target="_blank" rel="noreferrer" >
+                  <i className="nes-icon linkedin is-large"></i>
+                </a>
+                <a className="anchor__CV" href={cv} target="_blank" rel="noreferrer" >
+                  {/*               <!-- Copyright Nintendo --> */}
+                  <img className="nes-avatar link" src="https://res.cloudinary.com/diggrhtle/image/upload/v1605175715/persona%20page/link_cv6kti.gif" alt="CV" />
+                </a>
               </section>
             </Paper>
           </TimelineContent>
--- src/components/TimelineXSmall.js
+++ src/components/TimelineXSmall.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '../components/Modal';
-import LoanCalculator from './LoanCalculator';
-import WMW from './WMW';
-import BidraSammen from './BidraSammen';
-import JSToDo from '../components/JSToDo';
-import ReactToDo from '../components/ReactToDo';
 import cv from '../files/CV_Jorge_Moreno.pdf';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    textAlign:"center"
+    textAlign: "center"
   },
 }));
 
@@ -37,75 +31,75 @@ const TimelineXSmall = () => {
       description: "A Loan Calculator done with React.",
       gitHub: "https://github.com/jopemoma/loan-calculator",
       modal: true,
-      component: LoanCalculator,
+      link: "https://agitated-shaw-abd9ca.netlify.app/",
     },
     {
       name: "BidraSammen",
       description: "Our Graduation Project at Salt. An App done with React Native.",
       gitHub: "https://github.com/jopemoma/Graduation-Project",
       modal: true,
-      component: BidraSammen,
+      link: "https://res.cloudinary.com/diggrhtle/video/upload/v1604416370/persona%20page/Cloud0x9_Product_Demo_rjclga.mp4",
     },
     {
       name: "Which Movie is Worse?",
       description: "A game where you need to guess which movie has the worse rating! Done with React",
       gitHub: "https://github.com/jopemoma/Which-movie-is-worse-Game",
       modal: true,
-      component: WMW,
+      link: "https://which-movie-is-worse.herokuapp.com/",
     },
     {
       name: "React ToDo App",
       description: "A ToDo App done with React",
       gitHub: "https://github.com/jopemoma/React-ToDo-app",
       modal: true,
-      component: ReactToDo,
+      link: "https://ecstatic-shirley-082a22.netlify.app",
     },
     {
       name: "JavaScript ToDo App",
       description: "A ToDo App done with Vanilla JavaScript",
       gitHub: "https://github.com/jopemoma/Vanilla-JavaScript-ToDo-App",
       modal: true,
-      component: JSToDo,
+      link: "https://determined-varahamihira-fd0862.netlify.app/",
     },
   ]
 
   return (
     <div className="timeline" >
-        {projects.map((project, index) => (
-          <div key={index}>
-              <p className="smallStage" >STAGE {6 - index}</p>
-              <Paper elevation={3} className={classes.paper}>
-                <p className="project__title">{project.name} </p>
-                <p className="project">{project.description}</p>
-                <p className="project">You can check the repository on GitHub here: <a href={project.gitHub} className="nes-btn is-small is-primary" target="_blank" rel="noreferrer" >{project.name} </a> </p>
-                {project.modal &&
-                  <Modal project={project.component} />
-                }
-              </Paper>
-          </div>
-        ))}
-        <div id="contact">
-          <p className="smallStage" >STAGE 0</p>
-            <Paper elevation={3} className={classes.paper}>
-              <p className="project__title">Contact Information </p>
-              <img className="theHero" src="https://res.cloudinary.com/diggrhtle/image/upload/v1604332718/persona%20page/Jorge_Moreno_2_z9iur1.jpg" alt="The Hero" />
-              <section className="section__contact" >
-              <a href="https://github.com/jopemoma" target="_blank" rel="noreferrer" >
-                <i className="nes-icon github is-large"></i>
-              </a>
-              <a href="mailto:jorge.moreno@appliedtechnology.se" target="_blank" rel="noreferrer" >
-                <i className="nes-icon gmail is-large"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/jorge-moreno-mateo/" target="_blank" rel="noreferrer" >
-                <i className="nes-icon linkedin is-large"></i>
-              </a>
-              <a className="anchor__CV" href={cv} target="_blank" rel="noreferrer" >
-{/*               <!-- Copyright Nintendo --> */}
-                <img className="nes-avatar link" src="https://res.cloudinary.com/diggrhtle/image/upload/v1605175715/persona%20page/link_cv6kti.gif"  alt="CV" />
-              </a>
-              </section>
-            </Paper>
+      {projects.map((project, index) => (
+        <div key={index}>
+          <p className="smallStage" >STAGE {6 - index}</p>
+          <Paper elevation={3} className={classes.paper}>
+            <p className="project__title">{project.name} </p>
+            <p className="project">{project.description}</p>
+            <p className="project">You can check the repository on GitHub here: <a href={project.gitHub} className="nes-btn is-small is-primary" target="_blank" rel="noreferrer" >{project.name} </a> </p>
+            {project.modal &&
+              <a href={project.link} className="nes-btn is-success" target="_blank" rel="noreferrer" >Click here to see the Demo! </a>
+            }
+          </Paper>
         </div>
+      ))}
+      <div id="contact">
+        <p className="smallStage" >STAGE 0</p>
+        <Paper elevation={3} className={classes.paper}>
+          <p className="project__title">Contact Information </p>
+          <img className="theHero" src="https://res.cloudinary.com/diggrhtle/image/upload/v1604332718/persona%20page/Jorge_Moreno_2_z9iur1.jpg" alt="The Hero" />
+          <section className="section__contact" >
+            <a href="https://github.com/jopemoma" target="_blank" rel="noreferrer" >
+              <i className="nes-icon github is-large"></i>
+            </a>
+            <a href="mailto:jorge.moreno@appliedtechnology.se" target="_blank" rel="noreferrer" >
+              <i className="nes-icon gmail is-large"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/jorge-moreno-mateo/" target="_blank" rel="noreferrer" >
+              <i className="nes-icon linkedin is-large"></i>
+            </a>
+            <a className="anchor__CV" href={cv} target="_blank" rel="noreferrer" >
+              {/*               <!-- Copyright Nintendo --> */}
+              <img className="nes-avatar link" src="https://res.cloudinary.com/diggrhtle/image/upload/v1605175715/persona%20page/link_cv6kti.gif" alt="CV" />
+            </a>
+          </section>
+        </Paper>
+      </div>
     </div>
   );
 }
